cfd55db9abf2f57b9870c1dbaf8265d6ed1b94a9
coder-Rishi05 <rawatrishi1121@gmail.com>
2026-03-06 16:54:03 +0530

    added resume review

--- backend/src/controller/resume.controller.js
+++ backend/src/controller/resume.controller.js
@@ -1,3 +1,52 @@
+import { extractTextFromPdf } from "../services/pdf.service.js";
+import { generateEmailAndAnalysis } from "../services/ai.service.js";
+
 export const resumeAnalyse = async (req, res) => {
-  res.send("analysing resume");
+  if (!req.file) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Resume pdf is required" });
+  }
+
+  const { jobRole } = req.body;
+
+  if (!req.file) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Job role required" });
+  }
+
+  let resumeText;
+
+  try {
+    resumeText = await extractTextFromPdf(req.file.buffer);
+  } catch (error) {
+    return res.status(422).json({
+      success: false,
+      message: "Text extraction failed",
+      error: error.message,
+    });
+  }
+
+  let aiResult;
+
+  try {
+    aiResult = await generateEmailAndAnalysis(resumeText, jobRole);
+  } catch (error) {
+    return res.status(502).json({
+      success: false,
+      message: "AI response failed.",
+      error: error.message,
+    });
+  }
+
+  return res.status(200).json({
+    success: true,
+    analyses: aiResult.analysis,
+    meta: {
+      jobRole,
+      fileSize: `${(req.file.buffer.length / 1024).toFixed(2)}kb`,
+      fileName: req.file.originalname,
+    },
+  });
 };
--- backend/src/routes/resume.route.js
+++ backend/src/routes/resume.route.js
@@ -1,9 +1,9 @@
-import express from "express"
+import express from "express";
 import { resumeAnalyse } from "../controller/resume.controller.js";
-
+import upload from "../middleware/upload.middleware.js";
 
 const resumeRouter = express.Router();
 
-resumeRouter.post("/analyse",resumeAnalyse)
+resumeRouter.post("/analyse", upload.single("resume"), resumeAnalyse);
 
 export default resumeRouter;
